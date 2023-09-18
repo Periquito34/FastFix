a7afcd197d84206f6f82ec0a34a1b31aeee1dfb2
Periquito34 <andrespro2004@gmail.com>
2023-09-18 16:02:54 -0500

    views Empleado y Categoria

--- fastFixSystem/fastFix_app/views.py
+++ fastFixSystem/fastFix_app/views.py
@@ -1,5 +1,74 @@
-from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+#CategoriaServicio
+
+class CategoriaServicio(ListView):
+    model = CategoriaServicio
+
+class CategoriaServicioCrear(SuccessMessageMixin, CreateView):
+    model= CategoriaServicio
+    form= CategoriaServicio
+    fields= '__all__'
+    success_message= 'Categoria de Servicio creada exitosamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+class CategoriaServicioActualizar(SuccessMessageMixin, UpdateView):
+    model= CategoriaServicio
+    form= CategoriaServicio
+    fields= '__all__'
+    success_message= 'Categoria de Servicio actualizada exitosamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+class CategoriaServicioEliminar(SuccessMessageMixin, DeleteView):
+    model= CategoriaServicio
+    form= CategoriaServicio
+    fields= '__all__'
+    success_message= 'Categoria de Servicio eliminada exitosamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+class CategoriaServicioDetail(DetailView):
+    model= CategoriaServicio
 
 
+#Empleado
 
-# Create your views here.
+class Empleado(ListView):
+    model = Empleado
+
+class EmpleadoCrear(SuccessMessageMixin, CreateView):
+    model= Empleado
+    form= Empleado
+    fields= '__all__'
+    success_message= 'Empleado creado exitosamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+class EmpleadoActualizar(SuccessMessageMixin, UpdateView):
+    model= Empleado
+    form= Empleado
+    fields= '__all__'
+    success_message= 'Empleado actualizado exitosamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+class EmpleadoEliminar(SuccessMessageMixin, DeleteView):
+    model= Empleado
+    form= Empleado
+    fields= '__all__'
+    success_message= 'Empleado eliminado exitosamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+class EmpleadoDetail(DetailView):
+    model= Empleado
