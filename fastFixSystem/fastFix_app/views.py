be948d10a725655f75b83f82518b2e41c0f2af9c
Periquito34 <andrespro2004@gmail.com>
2023-09-18 16:23:46 -0500

    view Solicitud

--- fastFixSystem/fastFix_app/views.py
+++ fastFixSystem/fastFix_app/views.py
@@ -1,74 +1,37 @@
 
-def index(request):
-    return render(request, 'index.html')
+#Solcitud
 
-#CategoriaServicio
 
-class CategoriaServicio(ListView):
-    model = CategoriaServicio
+class Solicitud(ListView):
+    model = Solicitud
 
-class CategoriaServicioCrear(SuccessMessageMixin, CreateView):
-    model= CategoriaServicio
-    form= CategoriaServicio
+class SolicitudCrear(SuccessMessageMixin, CreateView):
+    model= Solicitud
+    form= Solicitud
     fields= '__all__'
-    success_message= 'Categoria de Servicio creada exitosamente'
+    success_message= 'Solicitud creada exitosamente'
 
     def get_success_url(self):
         return reverse('leer')
-
-class CategoriaServicioActualizar(SuccessMessageMixin, UpdateView):
-    model= CategoriaServicio
-    form= CategoriaServicio
+    
+class SolicitudActualizar(SuccessMessageMixin, UpdateView):
+    model= Solicitud
+    form= Solicitud
     fields= '__all__'
-    success_message= 'Categoria de Servicio actualizada exitosamente'
+    success_message= 'Solicitud actualizada exitosamente'
 
     def get_success_url(self):
         return reverse('leer')
-
-class CategoriaServicioEliminar(SuccessMessageMixin, DeleteView):
-    model= CategoriaServicio
-    form= CategoriaServicio
+    
+class SolicitudEliminar(SuccessMessageMixin, DeleteView):
+    model= Solicitud
+    form= Solicitud
     fields= '__all__'
-    success_message= 'Categoria de Servicio eliminada exitosamente'
+    success_message= 'Solicitud eliminada exitosamente'
 
     def get_success_url(self):
         return reverse('leer')
+    
+class SolicitudDetail(DetailView):
+    model= Solicitud
 
-class CategoriaServicioDetail(DetailView):
-    model= CategoriaServicio
-
-
-#Empleado
-
-class Empleado(ListView):
-    model = Empleado
-
-class EmpleadoCrear(SuccessMessageMixin, CreateView):
-    model= Empleado
-    form= Empleado
-    fields= '__all__'
-    success_message= 'Empleado creado exitosamente'
-
-    def get_success_url(self):
-        return reverse('leer')
-
-class EmpleadoActualizar(SuccessMessageMixin, UpdateView):
-    model= Empleado
-    form= Empleado
-    fields= '__all__'
-    success_message= 'Empleado actualizado exitosamente'
-
-    def get_success_url(self):
-        return reverse('leer')
-
-class EmpleadoEliminar(SuccessMessageMixin, DeleteView):
-    model= Empleado
-    form= Empleado
-    fields= '__all__'
-    success_message= 'Empleado eliminado exitosamente'
-
-    def get_success_url(self):
-        return reverse('leer')
-
-class EmpleadoDetail(DetailView):
-    model= Empleado
